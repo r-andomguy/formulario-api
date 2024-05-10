39f8f6e8506872ab016d64f60fc302424d6fc947
Bruno Coelho Ribas <85256618+r-andomguy@users.noreply.github.com>
2024-05-10 16:21:26 -0300

    melhoria no update.php

--- backend/api/update.php
+++ backend/api/update.php
@@ -1,25 +1,28 @@
 <?php
 
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: appplication/json');
-    header('Access-Control-Allow-Methods: PUT');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization');
 
-    include_once('../core/initialize.php');
-    
-    $post = new Post($db);
-    $data = json_decode(file_get_contents('php://input'));
-    
+include_once('../core/initialize.php');
+
+$post = new Post($db);
+$data = json_decode(file_get_contents('php://input'));
+
+$post->email = $data->email;
+
+// Verifica se o email existe no banco de dados
+if ($post->emailExists()) {
+
     $post->profile = $data->profile;
     $post->name = $data->name;
-    $post->age = $data->age;
-    $post->email = $data->email;
     $post->address = $data->address;
     $post->neighborhood = $data->neighborhood;
     $post->zipCode = $data->zipCode;
     $post->state = $data->state;
     $post->biography = $data->biography;
-    
+
     if ($post->update()) {
         echo json_encode(
             array('message' => 'Registro atualizado com sucesso.')
@@ -29,5 +32,10 @@
             array('message' => 'Não foi possível atualizar o registro.')
         );
     }
+} else {
+    echo json_encode(
+        array('message' => 'Email não encontrado. Não foi possível atualizar o registro.')
+    );
+}
 
 ?>
